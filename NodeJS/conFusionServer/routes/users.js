0b73e3ff220f5163cdc9494380563b5f67c05283
Nikita Buinovec <nikita.492@yandex.by>
2020-04-10 12:48:57 +0300

    Cors

--- NodeJS/conFusionServer/routes/users.js
+++ NodeJS/conFusionServer/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var authenticate = require('../authenticate');
-
+const cors = require('./cors');
 var passport = require('passport');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
@@ -10,7 +10,7 @@ var User = require('../models/user');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
   User.find({})
   .then((users) => {
       res.statusCode = 200;
@@ -20,19 +20,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next
   .catch((err) => next(err));
 });
 
-router.get('/logout', (req, res, next) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  } else {
-    var err = new Error('You are not logged in!');
-    err.status = 403;
-    next(err);
-  }
-});
-
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}),
     req.body.password, (err, user) => {
     if(err) {
@@ -63,11 +51,23 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
+});
+
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  } else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
 });
 
 module.exports = router;
